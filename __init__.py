8b9d286247cfe1d3a8cc09fb18021e350578f4f9
MouadBouharoun <96925712+MouadBouharoun@users.noreply.github.com>
2024-06-05 14:25:01 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+'''
+Filename: __init__.py
+Created: 2024-03-11
+
+Summary: The folder groups all the routes, under `routes` and import them in the router.py file.
+Each route should have its own route_name.py file that handles all the routes logic (get, post, delete...)
+Copyright (c) 2024 Henceforth
+'''
+
